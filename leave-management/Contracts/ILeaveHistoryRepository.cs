9382ddc3b43b548caebc8de2888c36addc25e5a0
UrvasheeJ <urvashee.jewootah@hangarww.com>
2020-10-08 16:38:06 +0400

    Add project files.

---
+++ leave-management/Contracts/ILeaveHistoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using leave_management.Data;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace leave_management.Contracts
+{ 
+    public interface ILeaveHistoryRepository : IRepositoryBase<LeaveHistory>
+    {
+    }
+}
